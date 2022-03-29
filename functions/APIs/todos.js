d67f3da45e1bbe506149d42c95c707c31e337835
Gioele <joelemaruccia@gmail.com>
2022-03-29 10:07:02 +0200

    Add first firebase configuration and files, ready for deploy

---
+++ functions/APIs/todos.js
@@ -0,0 +1,17 @@
+//todos.js
+
+exports.getAllTodos = (request, response) => {
+    todos = [
+        {
+            'id': '1',
+            'title': 'greeting',
+            'body': 'Hello world from sharvin shah'
+        },
+        {
+            'id': '2',
+            'title': 'greeting2',
+            'body': 'Hello2 world2 from sharvin shah'
+        }
+    ]
+    return response.json(todos);
+}
---
+++ functions/index.js
@@ -0,0 +1,11 @@
+//index.js
+
+const functions = require('firebase-functions');
+const app = require('express')();
+
+const {
+    getAllTodos
+} = require('./APIs/todos')
+
+app.get('/todos', getAllTodos);
+exports.api = functions.https.onRequest(app);
