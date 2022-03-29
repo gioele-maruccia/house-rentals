2ecffb08d2dbe5fc7a1b38ca1ef88898419d1764
Gioele <joelemaruccia@gmail.com>
2022-03-29 11:08:54 +0200

    Add todo cloud functions (add, edit, delete)

--- functions/index.js
+++ functions/index.js
@@ -4,8 +4,14 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const {
-    getAllTodos
+    getAllTodos,
+    postOneTodo,
+    deleteTodo,
+    editTodo
 } = require('./APIs/todos')
 
 app.get('/todos', getAllTodos);
+app.post('/todo', postOneTodo);
+app.delete('/todo/:todoId', deleteTodo);
+app.put('/todo/:todoId', editTodo);
 exports.api = functions.https.onRequest(app);
